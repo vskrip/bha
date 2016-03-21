a468fddea5de0dbdf607b6a8d40ab85d6842d7d1
vskrip <vskrip33@gmail.com>
2016-03-21 16:54:19 +0300

    Adding ProjectSchema to the /api/project/project.model.js of the  server

--- server/api/project/project.model.js
+++ server/api/project/project.model.js
@@ -2,10 +2,61 @@
 
 import mongoose from 'mongoose';
 
+var ObjectId = mongoose.Schema.Types.ObjectId;
+
 var ProjectSchema = new mongoose.Schema({
-  name: String,
-  info: String,
-  active: Boolean
+	field: {
+		type: String,
+		required: true
+	},
+	site: {
+		type: String,
+		required: true
+	},
+	well: {
+		type: String,
+		required: true
+	},
+	customer: String,
+	jobNumber: String,
+	startDate: { type: Date, default: Date.now },
+	finishDate: Date,
+	isCurrent: {
+		type: Boolean,
+		default: false
+	},
+	// tenant: {
+	// 	type: ObjectId,
+	// 	ref: 'User',
+	// 	index: true
+	// }
+	tenant: String
 });
 
+/**
+ * Virtuals
+ */
+
+// Public project information
+ProjectSchema
+  .virtual('title')
+  .get(function() {
+		return this.field + ' - ' + this.site + ' - ' + this.well;
+	}
+);
+
+/**
+ * Pre-save hook
+ */
+
+// Setting for all projects isCurrent to false
+ProjectSchema
+  .pre('save', function(next) {
+		if(this.isCurrent){
+			this.isCurrent = false;
+		}
+		next();
+	}
+);
+	
 export default mongoose.model('Project', ProjectSchema);
