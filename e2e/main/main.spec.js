45d2809699c1a95ccd2969127be0487bb2374d7d
vskrip <vskrip33@gmail.com>
2016-03-23 13:42:54 +0300

    Typo fixing

--- e2e/main/main.spec.js
+++ e2e/main/main.spec.js
@@ -3,16 +3,17 @@
 var config = browser.params;
 
 describe('Main View', function() {
-  var page;
+	var page;
 
-  beforeEach(function() {
-    browser.get(config.baseUrl + '/');
-    page = require('./main.po');
-  });
+	beforeEach(function() {
+		browser.get(config.baseUrl + '/');
+		page = require('./main.po');
+	});
 
-  it('should include jumbotron with correct data', function() {
-    expect(page.h1El.getText()).toBe('\'Allo, \'Allo!');
-    expect(page.imgEl.getAttribute('src')).toMatch(/yeoman.png$/);
-    expect(page.imgEl.getAttribute('alt')).toBe('I\'m Yeoman');
-  });
+	it('should include jumbotron with correct data', function() {
+		expect(page.h1El.getText()).toBe('Bottom Hole Assemblies');
+		expect(page.pEl.getText()).toBe('Web Application for BHA managment in the support process of drilling.');
+		expect(page.imgEl.getAttribute('src')).toMatch(/logo.png$/);
+		expect(page.imgEl.getAttribute('alt')).toBe('BHA Logo');
+	});
 });
