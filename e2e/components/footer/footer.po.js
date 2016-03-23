92967a73007f2b9ee2269f9f78ffff035ec1b129
vskrip <vskrip33@gmail.com>
2016-03-23 19:19:15 +0300

    Adding page objects of the footer component for e2e spec

---
+++ e2e/components/footer/footer.po.js
@@ -0,0 +1,15 @@
+/**
+ * This file uses the Page Object pattern to define the main page for tests
+ * https://docs.google.com/presentation/d/1B6manhG0zEXkC-H-tPo2vwU06JhL8w9-XCF9oehXzAQ
+ */
+
+'use strict';
+
+var FooterComponent = function() {
+	this.footer = element(by.css('.footer'));
+	this.footerCopy = this.footer.element(by.css('.footer-copy'));
+	this.footerMenu = this.footer.element(by.css('.footer-menu'));
+	this.footerEmail = this.footer.element(by.css('.footer-email'));
+};
+
+module.exports = new FooterComponent();
